0c05f863dcfcfc13e7e0ef5fba3dd4a84039c742
007AxelBerkati <bucel1916@gmail.com>
2022-05-12 15:25:53 +0700

    mempersiapkan componenet button part 2

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,22 +1,89 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/destructuring-assignment */
 import React from "react";
 import { Link } from "react-router-dom";
 import propTypes from "prop-types";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.isDisabled || props.isLoading) {
+    if (props.isDisabled) className.push("disabled");
+    return (
+      <span
+        className={className.join(" ")}
+        style={props.style}
+      >
+        {
+          props.isLoading ? <>
+            <span className="spinner-border spinner-border-sm mx-5" ></span>
+            <span className="sr-only">Loading....</span>
+          </>
+        }
+      </span>
+    );
+  }
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        // eslint-disable-next-line react/jsx-no-target-blank
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "_noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    }
+
+    return (
+      <Link
+        to={props.href}
+        className={className.join(" ")}
+        style={props.style}
+        onClick={onClick}
+      >
+        {props.children}
+      </Link>
+    );
+  }
+
   return (
-    <div>Button</div>
+    <button
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+
+    </button>
   );
 }
 
 Button.propTypes = {
   type: propTypes.oneOf(["button", "link"]),
-  onclick: propTypes.func,
+  onClick: propTypes.func,
   target: propTypes.string,
-  className : propTypes.string,
+  href: propTypes.string,
+  className: propTypes.string,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
-  isLarge:  propTypes.bool,
+  isLarge: propTypes.bool,
   isBlock: propTypes.bool,
-  hasShadow: propTypes.bool
+  hasShadow: propTypes.bool,
 };
